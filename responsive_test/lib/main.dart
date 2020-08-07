225829f4a861090b97ef7c844d5735d0b0a074d7
Gerson <contacto.gerson.mexico@gmail.com>
2020-08-07 18:12:48 -0500

    v1

--- responsive_test/lib/main.dart
+++ responsive_test/lib/main.dart
@@ -62,71 +62,77 @@ class _DashboardHomeState extends State<DashboardHome> {
   Widget buildBody(double width) {
     double elevation = 10;
   
-    return SingleChildScrollView(
-      physics: BouncingScrollPhysics(),
-      child: Wrap(
-        alignment: WrapAlignment.start,
-        crossAxisAlignment: WrapCrossAlignment.start,
-        runSpacing: 13,
-        spacing: 13,
-        children: <Widget>[
-          Card(
-            elevation: elevation,
-            child: Container(
-              color: Colors.indigo,
-              height: 250.0,
-              //take all X space
-             // width: double.infinity,
-            ),
-          ),
-         
-          Card(
+    return NotificationListener<OverscrollIndicatorNotification>(
+  onNotification: (OverscrollIndicatorNotification overscroll) {
+    overscroll.disallowGlow();
+    return false;
+  },
+          child: SingleChildScrollView(
+        //physics: BouncingScrollPhysics(),
+        child: Wrap(
+          alignment: WrapAlignment.start,
+          crossAxisAlignment: WrapCrossAlignment.start,
+          runSpacing: 13,
+          spacing: 13,
+          children: <Widget>[
+            Card(
               elevation: elevation,
               child: Container(
-                color: Colors.pink,
-                height: 300,
-                width: 362,
+                color: Colors.indigo,
+                height: 250.0,
+                //take all X space
+               // width: double.infinity,
               ),
             ),
+           
+            Card(
+                elevation: elevation,
+                child: Container(
+                  color: Colors.pink,
+                  height: 300,
+                  width: 362,
+                ),
+              ),
 
-             Card(
-            elevation: elevation,
-            child: Container(
-              color: Colors.purple,
-              width: 362,
-              height: 300,
-              
+               Card(
+              elevation: elevation,
+              child: Container(
+                color: Colors.purple,
+                width: 362,
+                height: 300,
+                
+              ),
             ),
-          ),
-          Card(
-            elevation: elevation,
-            child: Container(
-               decoration: BoxDecoration(
-                  border: Border.all(
-            color: Colors.black38,
-            width: 2.0,
-          ),
+            Card(
+              elevation: elevation,
+              child: Container(
+                 decoration: BoxDecoration(
+                    border: Border.all(
+              color: Colors.black38,
+              width: 2.0,
+            ),
 
-                   boxShadow: [
-            BoxShadow(
-              //si 1 es solido por eso usamos 15
-              blurRadius: 15,
-              spreadRadius: 3,
+                     boxShadow: [
+              BoxShadow(
+                //si 1 es solido por eso usamos 15
+                blurRadius: 15,
+                spreadRadius: 3,
 
-              color: Colors.white10,
-            ),
-          ],),
-                 //color: Colors.indigo,
-       
-              
-              height: 60.0,
-              //take all X space
-             // width: double.infinity,
-            ),
-          ),
+                color: Colors.white10,
+              ),
+            ],),
+                   //color: Colors.indigo,
          
-          
-        ],
+                
+                height: 60.0,
+                //take all X space
+               // width: double.infinity,
+              ),
+            ),
+           
+            
+          ],
+        ),
       ),
     );
   }
@@ -158,42 +164,45 @@ class _DashboardHomeState extends State<DashboardHome> {
         elevation: elevation,
         child: Container(
           color: Colors.blueGrey.shade700,
-          child: SingleChildScrollView(
-            child: Column(
-              
-              
-              children: <Widget>[
-                Center(
-                  child: Container(
-                    height: 130,
-                    
-                     decoration: BoxDecoration(color:
-                      Colors.blueGrey.shade900
+          
+          child: Scrollbar(
+                      child: SingleChildScrollView(
+              child: Column(
+                
+                
+                children: <Widget>[
+                  Center(
+                    child: Container(
+                      height: 130,
+                      
+                       decoration: BoxDecoration(color:
+                        Colors.blueGrey.shade900
+                        ),
+                      child: Center(
+                        child: CircleAvatar(
+                          radius: 35.0,
+                            backgroundImage: NetworkImage(
+                                'https://i.pinimg.com/474x/a8/6e/26/a86e26dffbcd0f8ffd0b7a6a4809ec68.jpg')),
                       ),
-                    child: Center(
-                      child: CircleAvatar(
-                        radius: 35.0,
-                          backgroundImage: NetworkImage(
-                              'https://i.pinimg.com/474x/a8/6e/26/a86e26dffbcd0f8ffd0b7a6a4809ec68.jpg')),
+                     
+                      
                     ),
-                   
-                    
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text("Gerson Morales", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                ),
-                buildListTile(0, Icons.home, 'Home'),
-                buildListTile(1, Icons.inbox, 'Inbox'),
-                buildListTile(2, Icons.delete, 'Trash'),
-                buildListTile(3, Icons.info, 'Spam'),
-                buildListTile(4, Icons.chat, 'Forums'),
-                buildListTile(5, Icons.flag, 'Updates'),
-                buildListTile(6, Icons.label, 'Promos'),
-                buildListTile(7, Icons.shopping_cart, 'Purchases'),
-                buildListTile(8, Icons.people, 'Social'),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text("Gerson Morales", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  ),
+                  buildListTile(0, Icons.home, 'Home'),
+                  buildListTile(1, Icons.inbox, 'Inbox'),
+                  buildListTile(2, Icons.delete, 'Trash'),
+                  buildListTile(3, Icons.info, 'Spam'),
+                  buildListTile(4, Icons.chat, 'Forums'),
+                  buildListTile(5, Icons.flag, 'Updates'),
+                  buildListTile(6, Icons.label, 'Promos'),
+                  buildListTile(7, Icons.shopping_cart, 'Purchases'),
+                  buildListTile(8, Icons.people, 'Social'),
+                ],
+              ),
             ),
           ),
         ),
